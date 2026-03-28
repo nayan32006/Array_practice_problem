7581f98797db75cf6c336a08d3f7cca016b9d4ba
nayan32006 <nayang389@gmail.com>
2026-03-28 10:10:07 +0530

    Day 1: basic of array

---
+++ array.cpp
@@ -0,0 +1,58 @@
+// introducing to array
+// array is an memory to store multiple variable in a single variable
+
+// progromme to perform aan array
+
+// array[4]= {23, 24, 26, 67};
+ // index of array [0][1][2][3]
+
+
+// CODE  :
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num[4]= {23, 24, 26, 67};
+    
+    cout <<" define in index:" <<endl;
+
+    cout <<num[0] <<endl <<"\n";
+    
+    cout <<" define in acording to fo loop:" <<endl;
+    
+
+    // using for loop
+
+    for(int i=0; i<4; i++){
+        cout <<num[i] <<endl;
+    }
+    return 0;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}  
